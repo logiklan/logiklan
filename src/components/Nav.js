3b5acf7f7b188a1d74d7bb93ce1d6b1d52b0299b
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-09-14 02:17:42 -0700

    start nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import logo from '../assets/logo.png'
+import styles from './Nav.css'
 
 const Nav = () => (
-  <div>
+  <ul className={styles.nav}>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/rules">The Rules</Link></li>
+    <li><Link to="/images">Image Gallery</Link></li>
+  </ul>
 )
+
+export default Nav
